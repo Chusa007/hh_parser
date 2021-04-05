183da5870d269dc3420f873063db42c3af24cf3c
rm.shamhalov <rm.shamhalov@tensor.ru>
2021-04-05 19:38:20 +0300

    Генерация запросов без пагинации

--- hh.py
+++ hh.py
@@ -7,13 +7,18 @@
 
 import requests
 import json
+import itertools
+from datetime import timedelta, date, datetime
 import filter
 from base import WorkPool
 
 
 class Hh(WorkPool):
 
+    # Токен приложения hh
     token = ''
+    # Максимальная страница вакансий
+    max_page = 19
 
     @staticmethod
     def check_token() -> bool:
@@ -38,16 +43,31 @@ class Hh(WorkPool):
         :param query_filter: фильтр для получения вакансий
         :return:
         """
-        url = f'https://api.hh.ru/vacancies?{query_filter}'
+        url = f'https://api.hh.ru/vacancies'
 
         res = requests.get(url=url,
+                           params=query_filter,
                            headers={'Authorization': f'Bearer {Hh.token}'})
 
         print(res.text)
+        print(res.url)
         ans = res.json().get("items")
         for i in ans:
             print(i.get("alternate_url"))
         # print(json.dumps(ans))
+
+    @staticmethod
+    def get_dict_info():
+        """
+        Метод для получения актуальной инфомарции по id фильтров
+        :return:
+        """
+        url = f'https://api.hh.ru/dictionaries'
+
+        res = requests.get(url=url,
+                           headers={'Authorization': f'Bearer {Hh.token}'})
+
+        print(res.json().get("employment"))
 
     @staticmethod
     def start_pool():
@@ -59,6 +79,33 @@ class Hh(WorkPool):
         print("задание начал")
 
 
+# print(Hh.get_dict_info())
 # print(Hh.check_token())
-# Hh.get_vacancies('salary=20000&per_page=2&page=5&only_with_salary=true')
+#qilter = {"salary": 20000, "per_page": 10, "page": 1, "only_with_salary": True}
+#Hh.get_vacancies(qilter)
+
+
+date_start = datetime.strptime(filter.DATE_FROM, "%Y-%m-%d").date()
+date_end = datetime.strptime(filter.DATE_TO, "%Y-%m-%d").date()
+delta = timedelta(days=3)
+a = []
+
+while date_start <= date_end:
+    current_date_end = date_start + delta
+    for sal, area, empl in itertools.product(filter.SALARY, filter.AREA, filter.EMPLOYMENT):
+        current_filter = {"salary": sal,
+                          "employment": empl,
+                          "area": area,
+                          "date_from": date_start.strftime("%Y-%m-%d"),
+                          "date_to": current_date_end.strftime("%Y-%m-%d"),
+                          "per_page": filter.PER_PAGE,
+                          "page": filter.PAGE,
+                          "only_with_salary": True}
+        print(current_filter)
+    break
+    # print(date_start.strftime("%Y-%m-%d"))
+    date_start = current_date_end
+#
+# print(len(a))
+# Hh.get_vacancies('salary=20000&per_page=100&page=19&only_with_salary=true')
 
